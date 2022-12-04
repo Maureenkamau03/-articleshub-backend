65cea945de47aeac4a461b2fae0295cedc40464c
Maureenkamau03 <mkaymaureen@gmail.com>
2022-12-04 03:10:48 +0300

    added different routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all 
+    render json: users
+  end
+
+  def show
+    user = User.find_by(params[:id])
+    render json: user
+  end
 
   def create
     user = User.create!(user_params)
-    render json: user
+    render json: user, status: :created
   end
 
 
 
   private
   def user_params
-    params.permit(:username, :email, :password_confirmation)
+    params.permit(:username, :email, :password, :password_confirmation)
   end
 end
